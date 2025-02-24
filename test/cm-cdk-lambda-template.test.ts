225b628f7b140fb70f9035bd2fda303d9c347122
igor-verumid <igor.jovic@verumid.com>
2025-02-24 12:55:18 +0100

    Initial commit

---
+++ test/cm-cdk-lambda-template.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CmCdkLambdaTemplate from '../lib/cm-cdk-lambda-template-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cm-cdk-lambda-template-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CmCdkLambdaTemplate.CmCdkLambdaTemplateStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
